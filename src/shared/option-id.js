6f7b0323712529ab30cdd043faab94468ff2d601
Albert Montagut Casero <albert.montagut.casero@gmail.com>
2022-04-25 11:41:15 +0200

    feat: utility methods added to option id

--- src/shared/option-id.js
+++ src/shared/option-id.js
@@ -4,3 +4,11 @@ export const OptionId = {
   bell: "bell",
   bar: "bar",
 };
+
+export function getOptionIdIndex(optionId){
+  return Object.keys(OptionId).findIndex(option => OptionId[option] === optionId);
+}
+
+export function getNextRailPosition(optionId){
+  return getOptionIdIndex(optionId) + Object.keys(OptionId).length;
+}
